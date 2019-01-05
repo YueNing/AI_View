0a6a6d701852305f81f97fd71cf15796a5568b59
uqveo <uqveo@student.kit.edu>
2019-01-05 18:44:16 +0100

    change appname to frontend bug fix

--- mk/frontend/urls.py
+++ mk/frontend/urls.py
@@ -1,7 +1,7 @@
 from django.conf.urls import url
 from django.urls import path
 from . import views
-app_name = 'mysample'
+app_name = 'frontend'
 urlpatterns = [
     # ex: /mysample/
     #url(r'^$', views.index, name='index'),

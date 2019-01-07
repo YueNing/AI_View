f5b494cb4260a937397ffeea0777c01377f0a5b0
uqveo <uqveo@student.kit.edu>
2019-01-08 00:57:29 +0100

    add pre_data scripts for downloader and imdb analyse

---
+++ pre_data/imdb_analyse.py
@@ -0,0 +1,25 @@
+import os
+
+from imdb import IMDb
+
+def get_genre(ia):
+    top250 = ia.get_top250_movies()
+    # Iterate through the first 20 movies in the top 250
+    for movie_count in range(0, 20):
+        # First, retrieve the movie object using its ID
+        movie = ia.get_movie(top250[movie_count].movieID)
+        # Print movie title and genres
+        print(movie['title'])
+        print(*movie['genres'], sep=", ")
+    
+def main():
+    ia = IMDb()
+    the_matrix = ia.get_movie('0133093')
+    #print(the_matrix['director'])
+    #print(ia.get_movie_infoset())
+    #dystopia = ia.get_k
+    #keyword('ai', results=5)
+    print(the_matrix['imdb'])
+    #get_genre(ia)
+if __name__ == "__main__":
+    main()

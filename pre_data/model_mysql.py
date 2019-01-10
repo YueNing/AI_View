6f146c424ae677eafed2746b4b83f3d141e6d1ae
Ubuntu <ubuntu@ip-172-31-83-103.ec2.internal>
2019-01-10 22:00:27 +0000

    add mysql table operation update,delete and insert

--- pre_data/model_mysql.py
+++ pre_data/model_mysql.py
@@ -45,16 +45,15 @@ class Module_mysql():
     def fetchone(self):
         return self.cursor.fetchone()
 
-    def insertTable(self, table_name, inserted_array):
+    def insertTable(self, table_name='', inserted_array={}):
         inserted = False
-        insert_val = response = []
+        response = {}
         if table_name:
             keys = ''
             values = ''
             for key, value in inserted_array.items():
                 keys += "`" + key + "`,"
                 values += "'"+value +"'," 
-                insert_val.append(value)
             keys = keys[:-1]
             values = values[:-1]
             sql = "INSERT INTO " +"`" +table_name + "` " + "("+ keys + ")" + " VALUES " + "(" + values + ")"
@@ -63,6 +62,57 @@ class Module_mysql():
             self.query(sql)
             self.commit()
             inserted = True
+        if inserted:
+            response = {"message": "inserted successfully"}
+        else:
+            response = {"message": "problem occured by inserted"}
+        return response
+    
+    def deleteTable(self, table_name='', where_att={}):
+        delete = False
+        reponse = {}
+        where_str = ''
+        if table_name:
+            for key, value in where_att.items():
+                where_str += " and `" + key + "` =" + "'" + value + "'"
+            sql = "DELETE FROM `" + table_name +"` where 1 " + where_str
+            #import pdb
+            #pdb.set_trace()
+            self.query(sql)
+            self.commit()
+            delete = True
+        if delete:
+            response = {"message": "delete successfully"}
+        else:
+            response = {"message": "problem occured by delete"}
+        return response
+
+    def updateTable(self, table_name='', update_val={}, where_att={}):
+        update = False
+        reponse = {}
+        where_str = ''
+        collum_str = ''
+        if table_name:
+            for key, value in update_val.items():
+                collum_str += "`"+key+"` ='" +value+"',"
+            collum_str = collum_str[:-1]
+            if where_att:
+                for key, value in where_att.items():
+                    where_str +=" and `"+key+"`='"+value+"'"
+            sql = "UPDATE "+table_name+" SET "+collum_str+" where 1 "+where_str
+            #import pdb
+            #pdb.set_trace()
+            self.query(sql)
+            self.commit()
+            update = True
+        else:
+            response = {"message":"provide valid table name"}
+            return response
+        if update:
+            response = {"message":"update successfully"}
+        else:
+            response = {"message":"unknow error"}
+        return response
 
 if __name__ == '__main__':
     con = {'host':'localhost',
@@ -71,15 +121,19 @@ if __name__ == '__main__':
             'db':'mk'
             }
     db = Module_mysql(con)
-    sql = "SELECT `id`, `password` FROM `users` WHERE `email`=%s"
+    sql = "SELECT `id`, `password`,`email` FROM `users` WHERE `password`=%s"
     test_sql = "INSERT INTO `users` (`email`,`password`) VALUES ('test@email.com','test12345')"
-    params = ('uqveo@student.kit.edu')
+    params = ('test123456')
     db.query(sql, params=params)
     result = db.fetchone()
     print(result)
     inserted_array = {"email":"test@email.com", "password":"test12345"}
-    db.insertTable('users', inserted_array)
-    db.query(sql, params=('test@email.com'))
+    #response = db.insertTable('users', inserted_array)
+    #print(response['message'])
+    #response = db.deleteTable(table_name='users', where_att={"email":"test@email.com"})
+    response = db.updateTable(table_name='users', update_val={"email":"n1085633848@outlook.com","password":"test123456"}, where_att={"password":"123456test"})
+    print(response)
+    db.query(sql, params=('test123456'))
     #db.commit()
     result = db.fetchall()
     print(result)
